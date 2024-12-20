cd3ba7496da85aee597c138894e36e4a378afa5b
Marglodis Tovar <marglodistovar@gmail.com>
2024-12-20 19:35:46 -0300

    Agregar un endpoint adicional para redirigir a listar productos

--- src/main/java/com/principal/controlador/ProductoControlador.java
+++ src/main/java/com/principal/controlador/ProductoControlador.java
@@ -6,27 +6,32 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.principal.modelo.Producto;
 
 @Controller
-@RequestMapping("/producto")
+@RequestMapping("/")
 public class ProductoControlador {
 	
 	private List<Producto> productos = new ArrayList<>();
 	private int idCounter = 1;
 	
-	@GetMapping("/agregar")
+	@RequestMapping("/")
+    public String redirectToShowProducts() {
+        return "redirect:/producto/mostrar";  // Redirige a la vista que muestra productos
+    }
+	
+	@RequestMapping("/producto/agregar")
 	public ModelAndView mostrarFormulario() {
 		ModelAndView mav = new ModelAndView("agregarProducto"); //Vista del formulario
 		mav.addObject("producto", new Producto(0,"","",0,0));
 		return mav;
 	}
 	
-    @PostMapping("/procesar")
+    @RequestMapping(value = "/producto/procesar", method = RequestMethod.POST)
     public ModelAndView procesarFormulario(@ModelAttribute Producto producto) {
         producto.setId(idCounter++); // Asignar un ID único
         productos.add(producto);
@@ -37,7 +42,7 @@ public class ProductoControlador {
         return mav;
     }
 
-    @GetMapping("/mostrar")
+    @RequestMapping("/producto/mostrar")
     public ModelAndView mostrarProductos() {
         ModelAndView mav = new ModelAndView("mostrarProductos"); // Vista con lista de productos
         mav.addObject("productos", productos); // Atributo con lista de productos
